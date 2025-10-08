6f355b7c29d83a4e42e9b64a1c8c8d4de688d5a9
GANESH_777 <95033308+GaneshWiz07@users.noreply.github.com>
2025-10-08 15:44:57 +0530

    Modified readme

--- HTML,CSS,JS BASICS/Sidebar Menu/index.js
+++
@@ -1,9 +0,0 @@
-const Bars=document.querySelector(".fa-bars");
-const Sidebar=document.querySelector(".sidebar");
-const Close=document.querySelector(".fa-times");
-Bars.addEventListener('click',()=>{
-    Sidebar.classList.toggle("show-sidebar");
-    });
-Close.addEventListener('click',()=>{
-    Sidebar.classList.remove("show-sidebar");
-});
